85a7795157fd018b56bff98c34873da571e2555a
j0uce <mercier.jocelyn@ymail.com>
2022-06-18 12:26:43 +0200

    jouce

---
+++ script.js
@@ -0,0 +1,27 @@
+//Day mode
+
+// const switchBtn = document.querySelector(".day");
+// const dayBg = document.querySelector(".header");
+// const li = document.getElementsByClassName('li');
+// console.log(li);
+
+
+
+// // switchBtn.addEventListener("click", () => {
+   
+// //     if(switchBtn.classList.contains('day')){
+// //         switchBtn.classList.add('night');
+// //         switchBtn.classList.remove('day');
+// //         dayBg.style.background = "url(./img/15.jpg) center/cover";
+// //         dayBg.style.color = "#3a2901";
+        
+        
+        
+// //     } else if(switchBtn.classList.contains('night')){
+// //         switchBtn.classList.add('day');
+// //         switchBtn.classList.remove('night');
+// //         dayBg.style.background = "url(./img/13.jpg) center/cover";
+// //         dayBg.style.color = "whitesmoke";
+// //     }
+// // });
+
