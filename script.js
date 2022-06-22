1be718b8b6dcc75982b9b92f03254951f5e52e55
j0uce <mercier.jocelyn@ymail.com>
2022-06-22 23:37:03 +0200

    english corrective

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let englishMode = document.getElementById('english');
 
 englishMode.addEventListener("click", () => {
     document.getElementById("header-p").innerHTML = "Self-taught and very invested, I would like to improve my skills by working for a company that would be willing to train me";
+    document.getElementById("webdev").innerHTML = "Web Developer Front-end";
     document.getElementById("btn").innerHTML = "Read more";
     document.getElementById("about-h2").innerHTML = "About me";
+    document.getElementById("webdev2").innerHTML = "Web Developer";
     document.getElementById("about-p").innerHTML = "My name is Jocelyn, I am in professional reconversion with the aim of becoming a Web Developer. After having done the Piscine of the school 42 in Lyon in 2021, I took a taste for programming, since then, I have been trying to improve my skills. Self-taught and very invested, I would like to improve my skills by working for a company that would be willing to train me.";
     document.getElementById("solve").innerHTML = "Problem solving";
     document.getElementById("solve-p").innerHTML = "it's very stimulating for me to analyze a problem in order to find a solution";
@@ -22,5 +24,5 @@ englishMode.addEventListener("click", () => {
     document.getElementById("mail").innerHTML = "Mail";
     document.getElementById("adress").innerHTML = "Adress";
     document.getElementById("network").innerHTML = "Network";
-    
+    document.getElementById("learn").innerHTML = "I would like to learn :";
 })
