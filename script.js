48f0325d0d59f113c1081de701f9b5167835f8db
j0uce <mercier.jocelyn@ymail.com>
2022-06-22 23:27:50 +0200

    english mode

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-//Day mode
+//English 
 
-// const switchBtn = document.querySelector(".day");
-// const dayBg = document.querySelector(".header");
-// const li = document.getElementsByClassName('li');
-// console.log(li);
+let englishMode = document.getElementById('english');
 
-
-
-// // switchBtn.addEventListener("click", () => {
-   
-// //     if(switchBtn.classList.contains('day')){
-// //         switchBtn.classList.add('night');
-// //         switchBtn.classList.remove('day');
-// //         dayBg.style.background = "url(./img/15.jpg) center/cover";
-// //         dayBg.style.color = "#3a2901";
-        
-        
-        
-// //     } else if(switchBtn.classList.contains('night')){
-// //         switchBtn.classList.add('day');
-// //         switchBtn.classList.remove('night');
-// //         dayBg.style.background = "url(./img/13.jpg) center/cover";
-// //         dayBg.style.color = "whitesmoke";
-// //     }
-// // });
-
+englishMode.addEventListener("click", () => {
+    document.getElementById("header-p").innerHTML = "Self-taught and very invested, I would like to improve my skills by working for a company that would be willing to train me";
+    document.getElementById("btn").innerHTML = "Read more";
+    document.getElementById("about-h2").innerHTML = "About me";
+    document.getElementById("about-p").innerHTML = "My name is Jocelyn, I am in professional reconversion with the aim of becoming a Web Developer. After having done the Piscine of the school 42 in Lyon in 2021, I took a taste for programming, since then, I have been trying to improve my skills. Self-taught and very invested, I would like to improve my skills by working for a company that would be willing to train me.";
+    document.getElementById("solve").innerHTML = "Problem solving";
+    document.getElementById("solve-p").innerHTML = "it's very stimulating for me to analyze a problem in order to find a solution";
+    document.getElementById("team-work").innerHTML = "Team work";
+    document.getElementById("team-workp").innerHTML = "i'm able to work in a team and i can speak in public";
+    document.getElementById("adaptability").innerHTML = "Adaptability";
+    document.getElementById("adaptability-p").innerHTML = "I am able to adapt to all types of situations and to manage my stress";
+    document.getElementById("curiosity").innerHTML = "Curiosity";
+    document.getElementById("curiosity-p").innerHTML = "Curious by nature, I love learning new knowledge and skills";
+    document.getElementById("hire-p").innerHTML = "I will show motivation and determination during my training. My previous job led me to have a lot of responsibilities, I am ambitious by nature and I am convinced that I could be a real asset to your company.";
+    document.getElementById("hire").innerHTML = "Why me ?";
+    document.getElementById("contactme").innerHTML = "Contact me";
+    document.getElementById("phone").innerHTML = "Phone number";
+    document.getElementById("mail").innerHTML = "Mail";
+    document.getElementById("adress").innerHTML = "Adress";
+    document.getElementById("network").innerHTML = "Network";
+    
+})
